20e5ca70041b6aa92bfa4e40b6cbabe92e6e64c0
drnachio <nacho@binpar.com>
2020-09-01 16:47:04 +0200

    fix

--- tests/data/getPokemons.ts
+++ tests/data/getPokemons.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
-test('Get Pokemon Info', async (): void => {
-  const result = await axios.get('https://pokeapi.co/api/v2/');
-  console.log(result.data);
-  expect(1 + 2).toBe(3);
+interface PokemonList {
+  count: number;
+}
+test('Get Pokemon Info', async (): Promise<void> => {
+
+  const result = await axios.get<PokemonList>('https://pokeapi.co/api/v2/pokemon?limit=1000');
+  const {data} = result;  
+  expect(data.count).toBe(1050);
 });
